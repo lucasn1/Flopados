d4b46fa412c410d4ce66a2cde1342214e341a2b3
lucasn1 <lucas.sadelima16@gmail.com>
2020-07-24 15:28:49 -0300

    Add files via upload

---
+++ Painel/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/1060ff926e.js" crossorigin="anonymous"></script>
+    <title>Flopados</title>
+</head>
+<body>
+    <div class="box-left">
+		<div class="header">
+			<div class="header-img"></div>
+			<div class="profile">
+				<div class="online"></div>
+			</div>
+			<div class="notification">
+				<p><img src="img/bell-outline.svg" alt=""></p>
+			</div>
+			<div class="info">
+				<div class="user">vicsacramento</div>
+				<div class="role">Webmaster</div>
+			</div>
+			<div class="boneco"></div>
+		</div>
+		<div class="menu">
+        <ul>
+            <li><a href="#"><i class="fas fa-home"></i>Homepage</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>Meus dados</a></li>
+            <li><a href="#"><i class="fas fa-star"></i>Administração</a></li>
+            <li><a href="#"><i class="fas fa-star"></i>Coordenação</a></li>
+            <li><a href="#"><i class="fas fa-star"></i>Moderação</a></li>
+            <li><a href="#"><i class="fas fa-file-invoice"></i>Portal de notícias</a></li>
+			<li><a href="#"><i class="fas fa-headphones-alt"></i>Locução</a></li>
+			<li><a href="#"><i class="fas fa-server"></i>Logs</a></li>
+			<li><a href="#"><i class="fas fa-sign-out-alt"></i>Deslogar-se</a></li>
+        </ul> 
+		</div>
+    </div>
+</body>
+</html>
